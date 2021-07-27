0cdd5ac3c35f9b180e111bbbb89776adfe8ece31
xiongxiao01 <xiongxiao01@corp.netease.com>
2021-07-27 14:25:53 +0800

    补充对hover的处理

--- src/drag-list/drag-item/index.jsx
+++ src/drag-list/drag-item/index.jsx
@@ -1,62 +1,73 @@
-import React, { useRef } from 'react'
-import { useDrag, useDrop } from 'react-dnd'
-import './index.css'
+import React, { useRef, useState } from "react";
+import { useDrag, useDrop } from "react-dnd";
+import "./index.css";
 
 function throttle(fn) {
-  let timer = null
+  let timer = null;
   return (...args) => {
     if (timer) {
-      return
+      return;
     }
     timer = window.setTimeout(() => {
-      fn(...args)
-      timer = null
-    }, 100)
-  }
+      fn(...args);
+      timer = null;
+    }, 300);
+  };
 }
 
 const DragItem = ({ value, index }) => {
-  const ref = useRef()
+  const ref = useRef();
+  const [canDrag, setCanDrag] = useState(false);
 
-  const [{ isDragging }, drag] = useDrag({
-    type: 'songItem',
-    item: {
-      value,
-      index,
+  const [{ isDragging }, drag] = useDrag(
+    {
+      type: "songItem",
+      item: {
+        value,
+        index,
+      },
+      canDrag: () => {
+        return canDrag;
+      },
+      collect: (monitor) => {
+        return {
+          isDragging: monitor.isDragging(),
+        };
+      },
     },
-    collect: (monitor) => {
-      return {
-        isDragging: monitor.isDragging(),
-      }
-    },
-  })
+    [canDrag]
+  );
 
   const [{}, drop] = useDrop(() => {
     return {
-      accept: 'songItem',
+      accept: "songItem",
       hover: throttle((item, monitor) => {
-        if (!ref.current || item.index === index) return
+        if (!ref.current || item.index === index) return;
 
-        const offset = monitor.getClientOffset()
-        if (!offset) return
-        const dropAreaRect = ref.current.getBoundingClientRect()
-        const middleY = (dropAreaRect.top + dropAreaRect.bottom) / 2
-        const mouseY = offset.y
+        const offset = monitor.getClientOffset();
+        if (!offset) return;
+        const dropAreaRect = ref.current.getBoundingClientRect();
+        const middleY = (dropAreaRect.top + dropAreaRect.bottom) / 2;
+        const mouseY = offset.y;
         // down
         if (item.index < index && mouseY > middleY) {
-          console.log('移位', 'dragged', item.index, 'drop', index)
-          return
+          // do moveItem
+          console.log("移位", "dragged", item.index, "drop", index);
+          item.index = index
+          return;
         }
 
         if (item.index > index && mouseY > middleY) {
-          console.log('移位', 'dragged', item.index, 'drop', index)
-          return
+          // do moveItem
+          console.log("移位", "dragged", item.index, "drop", index);
+          item.index = index
+          return;
         }
       }),
-    }
-  })
+    };
+  });
 
-  drop(drag(ref))
+  drop(drag(ref));
 
   return (
     <div
@@ -65,10 +76,26 @@ const DragItem = ({ value, index }) => {
       style={{
         opacity: isDragging ? 0 : 1,
       }}
+      onDragEnd={() => {
+        console.log('drag end')
+        setCanDrag(false);
+      }}
     >
+      <div
+        style={{
+          display: "inline-block",
+          width: 50,
+          height: 50,
+          cursor: "move",
+          background: "red",
+        }}
+        onMouseDown={() => {
+          setCanDrag(true);
+        }}
+      ></div>
       {value}
     </div>
-  )
-}
+  );
+};
 
-export default DragItem
+export default DragItem;
